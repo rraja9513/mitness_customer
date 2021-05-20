6dbcaad42273ef260235a7fffccf3a08183cdf61
Raja R <rraja9513@gmail.com>
2021-05-20 17:20:23 +0530

    searchadded

--- routes/customers.js
+++ routes/customers.js
@@ -6,7 +6,7 @@ router.route('/').get((req, res) => {
       .then(customers => res.json(customers))
       .catch(err => res.status(400).json('Error: ' + err));
   });
-  router.route('/search').get((req, res) => {
+  router.route('/search').post((req, res) => {
     Customer.find({firstname : req.body.firstname})
       .then(customers => res.json(customers))
       .catch(err => res.status(400).json('Error: ' + err));
